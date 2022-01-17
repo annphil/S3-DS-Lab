81ad124759a384904fcdd7f7094871325a130f1f
annphil <75479202+annphil@users.noreply.github.com>
2022-01-17 16:34:08 +0530

    Using auxiliary stack, Linked List non usable

--- 3.4sortedPushStack.c
+++ 3.4sortedPushStack.c
@@ -1,80 +1,49 @@
 #include<stdio.h>
 #include <stdlib.h>
+#define MAX_SIZE 100
+int stack1[MAX_SIZE], stack2[MAX_SIZE];
+int top1 = -1, top2 = -1;
 
-typedef struct {
-	int data;
-	struct listnode* link;
-}listnode;
-
-listnode* first = NULL;
-
-listnode* createNode(int value){
-	listnode* node = (listnode*)malloc(sizeof(listnode));
-	node->data = value;
-	node->link = NULL;
-	return node;
+void push(int *stack, int *top, int element){
+	if(*top == MAX_SIZE-1)
+		printf("Stack overflow\n");
+	else
+		stack[++*top] = element;
 }
 
-void insertNode(int value){
-	listnode* node = createNode(value);
-	if( first->data == NULL )
-		first->data = value;
-	else if( value <= first->data ){
-		node->link = first;
-		first = node;
+int pop(int *stack, int *top){
+	if(*top == -1){
+		return -1;
 	}
-	else if( value > first->data){
-		listnode* temp1 = first->link;
-		listnode* temp2 = temp1->link;
-		if( first->link == NULL )
-			first->link = node;
-		else if( value <= temp1->data){    // When there are just 2 nodes in list
-			first->link = node;
-			node->link = temp1;
+	else
+		return stack[(*top)--];
+}
+
+void sortedPushStack(int element){
+	if( element >= stack1[top1] || top1==-1 )
+		push(stack1, &top1, element);
+	else {
+		while( element<stack1[top1] && top1!=-1 ){
+			stack2[++top2] = pop(stack1, &top1);
 		}
-		else {
-			while(temp2->link ){
-				if(value >= temp1->data && value <= temp2->data ){
-					temp1->link = node;
-					node->link = temp2;
-					break;	
-				}
-				else {
-					temp2 = temp2->link;
-					temp1 = temp1->link;
-				}	
-			}
-			if(temp2->link == NULL && value >= temp1->data && value <= temp2->data){
-				temp1->link = node;
-				node->link = temp2;
-			}
-			else if(temp2->link == NULL && value > temp2->data)
-				temp2->link = node;	
-		}		
-	}			
-}
-
-void display(){
-	listnode* temp = first;
-	while(temp){
-		printf("%d\t", temp->data);
-		temp = temp->link;
+		push(stack1, &top1, element);
+		while(top2!=-1){
+			stack1[++top1] = pop(stack2, &top2);
+		}
 	}
-	printf("%d\t", temp->data);
-}	
+}
 
 void main(){
-	int i, n, element ;
+	int i, n, element;
 	printf("Enter no of elements to be pushed: ");
 	scanf("%d", &n);
 	for(i=0; i<n; i++){
 		printf("Enter element to be pushed: ");
 		scanf("%d", &element);
-		insertNode(element);
+		sortedPushStack(element);
 	}
+	printf("Sorted elements: ");
 	for(i=0; i<n; i++){
-		printf("Sorted elements: ");
-		display();
-	}
-		
+		printf("%d ", stack1[i]);
+	}			
 }
