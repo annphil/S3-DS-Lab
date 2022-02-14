7b3f24f6450d1d5a0295b4d5c131faaa10f99b8a
annphil <75479202+annphil@users.noreply.github.com>
2022-02-14 06:46:41 +0000

    Create LinkedList2.c

---
+++ Cycle4/LinkedList2.c
@@ -0,0 +1,86 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+#define MAX_SIZE 100
+struct listNode{
+	int data;
+	struct listNode *link;
+};
+
+struct listNode *head = NULL;
+
+void insertEnd(int data){
+	struct listNode *node = (struct listNode*)malloc(sizeof(struct listNode));
+	node->data = data;
+	node->link = NULL;
+	
+	if(head == NULL)
+		head = node;
+	else{
+		struct listNode *temp;
+		temp = head;
+		while(temp->link){
+			temp = temp->link;
+		}
+		temp->link = node;
+	}	
+}
+
+void insert(int data, int check){
+	struct listNode *node = (struct listNode*)malloc(sizeof(struct listNode));
+	node->data = data;
+	node->link = NULL;
+	
+	struct listNode *temp = head;
+	if(head->data == check){
+		head = head->link;
+		free(temp);
+	}
+	else{
+		struct listNode *trail = head;
+		while(trail->link){
+			if(temp->data == check){
+				trail->link = temp->link;
+				free(temp);
+				break;
+			}
+			else
+				trail = temp;
+				temp = temp->link;
+		}	
+	}
+}
+
+void displayList(){
+	struct listNode *temp = head;
+	while(temp->link){
+		printf("%d ", temp->data);
+		temp = temp->link;
+	}
+	printf("%d\n", temp->data);	
+}
+
+void main(){
+	int n, flag = 1;
+	while(flag!=0){
+		printf("\nEnter choice\n1. Insert at end\n4. Insert after a node\n3. Display List\n4. Exit\n");
+		scanf("%d", &n);
+		switch (n){
+			case 1 : printf("Enter data to be inserted:");
+				 int data;
+				 scanf("%d ", &data);
+				 insertEnd(data);
+				 break;
+			case 2 : printf("Enter data and no. after which to be inserted:");
+				 int dat, check;
+				 scanf("%d %d", &dat, &check);
+				 insert(dat, check);
+				 break;
+			case 3 : printf("Linked List: ");
+				 displayList();
+				 break;
+			case 4 : flag = 0;
+				 break;
+		}
+	}
+}
